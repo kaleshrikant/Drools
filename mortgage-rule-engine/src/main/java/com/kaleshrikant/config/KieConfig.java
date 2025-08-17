e37be0cb824ae42c8147ad8f6d65b81d24444c75
Shrikant Kale <kaleshrikant@live.com>
2025-08-17 21:31:17 +0530

    removed ReleaseId from KieConfig

--- mortgage-rule-engine/src/main/java/com/kaleshrikant/config/KieConfig.java
+++ mortgage-rule-engine/src/main/java/com/kaleshrikant/config/KieConfig.java
@@ -1,7 +1,9 @@
 package com.kaleshrikant.config;
 
 import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +16,18 @@ import org.springframework.context.annotation.Configuration;
 public class KieConfig {
 
 	@Bean
-	public KieContainer kieContainer() {
-		return KieServices.Factory.get().getKieClasspathContainer();
+	public KieServices kieServices() {
+		return KieServices.Factory.get();
+	}
+
+	@Bean
+	public KieContainer kieContainer(KieServices kieServices) {
+		return kieServices.getKieClasspathContainer();
+	}
+
+	@Bean
+	public KieSession kieSession(KieContainer kieContainer) {
+		return kieContainer.newKieSession("mortgage-rule-engine");
 	}
 
 }
