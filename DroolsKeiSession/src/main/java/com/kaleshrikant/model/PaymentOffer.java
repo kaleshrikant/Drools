b2250205c6dae8bcd2e09f2055da0b185cc7226b
Shrikant Kale <kaleshrikant@live.com>
2025-08-17 10:44:46 +0530

    added model

---
+++ DroolsKeiSession/src/main/java/com/kaleshrikant/model/PaymentOffer.java
@@ -0,0 +1,25 @@
+package com.kaleshrikant.model;
+
+/**
+ * @author Shrikant Kale
+ * @Date 8/17/25
+ */
+
+public class PaymentOffer {
+	private String channel;
+	private int discount;
+
+
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+}
