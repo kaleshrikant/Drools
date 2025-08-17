cb4f45bec97dd750c0443488b2b88ee2f67b2082
Shrikant Kale <kaleshrikant@live.com>
2025-08-17 12:21:49 +0530

    added Classes

--- ComplexAndMultipleRules/src/main/java/com/kaleshrikant/Main.java
+++ ComplexAndMultipleRules/src/main/java/com/kaleshrikant/Main.java
@@ -1,10 +1,41 @@
 package com.kaleshrikant;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.kaleshrikant.model.PaymentOffer;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+
 public class Main {
 	public static void main(String[] args) {
-	}
+		// 1. Creating : Service
+			KieServices services = KieServices.Factory.get();
 
+		// 2. Creating : Container
+			KieContainer container= services.getKieClasspathContainer();
+
+		// 3. Creating : Session
+			KieSession session = container.newKieSession("complex-ksession-rule");
+
+		// 4. Creating : Facts
+		PaymentOffer payTmOffer = new PaymentOffer();
+			payTmOffer.setChannel("PayTm");
+			payTmOffer.setFestival("Diwali");
+			payTmOffer.setFirstTimeCustomer(true);
+
+		// 4. Inserting : Facts
+			FactHandle factHandle = session.insert(payTmOffer);
+
+		// 5. Firing : Rules
+			session.fireAllRules();
+
+	System.out.println("The cashback for this payment channel VIA KIE "+payTmOffer.getChannel()+" is "+payTmOffer.getDiscount());
+
+		/*
+			First time customer
+			The cashback for this payment channel VIA KIE PayTm is 10
+		*/
+	}
 
 }
---
+++ ComplexAndMultipleRules/src/main/java/com/kaleshrikant/model/PaymentOffer.java
@@ -0,0 +1,45 @@
+package com.kaleshrikant.model;
+
+/**
+ * @author Shrikant Kale
+ * @Date 8/17/25
+ */
+
+public class PaymentOffer {
+	private String channel;
+	private int discount;
+	private String festival;
+	private boolean firstTimeCustomer;
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public String getFestival() {
+		return festival;
+	}
+
+	public void setFestival(String festival) {
+		this.festival = festival;
+	}
+
+	public boolean isFirstTimeCustomer() {
+		return firstTimeCustomer;
+	}
+
+	public void setFirstTimeCustomer(boolean firstTimeCustomer) {
+		this.firstTimeCustomer = firstTimeCustomer;
+	}
+}
