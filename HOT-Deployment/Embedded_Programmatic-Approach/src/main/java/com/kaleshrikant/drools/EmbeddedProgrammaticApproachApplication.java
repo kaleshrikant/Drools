d89a2bdf431fe1cdcca2e916839768cac1773141
Shrikant Kale <kaleshrikant@live.com>
2025-09-28 14:35:09 +0530

    Embedded/Programmatic Approach

---
+++ HOT-Deployment/Embedded_Programmatic-Approach/src/main/java/com/kaleshrikant/drools/EmbeddedProgrammaticApproachApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedProgrammaticApproachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedProgrammaticApproachApplication.class, args);
+	}
+
+}
